8173d2471da93e523fce59407aa32e58465f16f6
ayush9951 <ayushgupta9951@gmail.com>
2021-04-15 15:54:37 +0530

    Add Hospital Section

--- Hospital_App/urls.py
+++ Hospital_App/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from Hospital_App.views import home
+from Hospital_App.views import home, hospital
 urlpatterns = [
     path('', home),
+    path('hospital', hospital, name = 'hospital-url')
 ]
